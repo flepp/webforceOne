779f225080d039aa9b1a9ad2a5810350d74131ef
philippetasch <flepptasch@gmail.com>
2016-06-01 14:59:03 +0200

    initial commit

---
+++ inc/header.php
@@ -0,0 +1,9 @@
+<?php 
+
+	try {
+		$dsn = 'mysql:dbname=DBB;host=localhost;';
+		$pdo = new PDO($dsn, 'pseudo', 'pass');
+	} catch (Exception $e) {
+		echo 'Error: ' $e->getmessage();
+	}
+ ?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>WEBFORCE ONE</title>
+</head>
+<body>
+	<p>Philippe was here</p>
+</body>
+</html>
