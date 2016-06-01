d804a3dd231001fd11a3dce470353c44a3f61187
totossj4 <ibrahima.thiello@hotmail.fr>
2016-06-02 00:00:51 +0200

    search almost finished

--- search.php
+++ search.php
@@ -2,6 +2,22 @@
 
 require 'inc/db.php';
 
+$sql = '
+	SELECT mov_id, mov_title, mov_cast, mov_synopsis, mov_path, mov_original_title, mov_image, cat_name, sto_name, mov_date_creation
+			FROM movie
+			LEFT OUTER JOIN category ON  category.cat_id = movie.cat_id
+			LEFT OUTER JOIN storage ON  storage.sto_id = movie.sto_id
+';
+$pdoStatement = $pdo->prepare($sql);
+
+	if($pdoStatement->execute() === false){
+		print_r($pdo->errorInfo());
+	}
+	else if($pdoStatement->rowCount() > 0){
+		$searchList2 = $pdoStatement->fetchAll();
+	}
+
+
 //récupèration et vérification des données du form de l'index en GET
 if(!empty($_GET['search']) && isset($_GET['search'])){
 	$search = $_GET['search'];
@@ -11,31 +27,98 @@ if(!empty($_GET['search']) && isset($_GET['search'])){
 
 	//Lancement requête de lecture
 	$sqlSearchResult = '
-		SELECT mov_id, mov_title, mov_cast, mov_synopsis, mov_path, mov_original_title, mov_image
+		SELECT mov_id, mov_title, mov_cast, mov_synopsis, mov_path, mov_original_title, mov_image, cat_name, sto_name, mov_date_creation
 		FROM movie
 		LEFT OUTER JOIN category ON  category.cat_id = movie.cat_id
 		LEFT OUTER JOIN storage ON  storage.sto_id = movie.sto_id
-		OR mov_title LIKE : recherche
-		OR mov_original_title LIKE : recherche
-		OR mov_synopsis LIKE : recherche
-		OR mov_path LIKE : recherche
-		OR mov_cast LIKE : recherche
+		WHERE mov_title LIKE :recherche
+		OR mov_original_title LIKE :recherche
+		OR mov_synopsis LIKE :recherche
+		OR mov_path LIKE :recherche
+		OR mov_cast LIKE :recherche
 
 	';
 
 	//préparation de la requête et excecution
 	$pdoStatement = $pdo->prepare($sqlSearchResult);
 
-	$pdoStatement->bindValue('recherche', '%'.$search.'%');
+	$pdoStatement->bindValue(':recherche', '%'.$search.'%');
 	if($pdoStatement->execute() === false){
 		print_r($pdo->errorInfo());
 	}
 	else if($pdoStatement->rowCount() > 0){
 		$searchList = $pdoStatement->fetchAll();
+		//print_r($searchList);
 		$nbRows = $pdoStatement->rowCount();
 	}
 
 }
+
+
+
+//récupèration et vérification des données du form de l'index en GET
+$formTwoValid = false;
+$catSearch ='';
+$titleSearch ='';
+$actorSearch ='';
+$stoSearch ='';
+$dateSearch ='';
+
+if(!empty($_GET['catSearch']) || !empty($_GET['titleSearch']) || !empty($_GET['actorSearch']) || !empty($_GET['stoSearch']) || !empty($_GET['dateSearch'])){
+	$formTwoValid = true;
+	print_r($_GET);
+	if(!empty($_GET['catSearch'])){
+		$catSearch = $_GET['catSearch'];
+	}
+	if(!empty($_GET['titleSearch'])){
+		$titleSearch = $_GET['titleSearch'];
+	}
+	if(!empty($_GET['actorSearch'])){
+		$actorSearch = $_GET['actorSearch'];
+	}
+	if(!empty($_GET['stoSearch'])){
+		$stoSearch = $_GET['stoSearch'];
+	}
+	if(!empty($_GET['dateSearch'])){
+		$dateSearch = $_GET['dateSearch'];
+	}
+
+	//tableau pour récupèrer mes données
+	$searchList3 = array();
+
+	//Lancement requête de lecture
+	$sqlSearchResultAff = '
+		SELECT mov_id, mov_title, mov_cast, mov_synopsis, mov_path, mov_original_title, mov_image, cat_name, sto_name, mov_date_creation, mov_cast
+		FROM movie
+		LEFT OUTER JOIN category ON  category.cat_id = movie.cat_id
+		LEFT OUTER JOIN storage ON  storage.sto_id = movie.sto_id
+		OR cat_name LIKE :catesearch
+		OR mov_title LIKE :titlesearch
+		OR mov_cast LIKE :actorsearch
+		OR sto_name LIKE :stosearch
+		OR mov_date_creation LIKE :datesearch
+
+	';
+
+	//préparation de la requête et excecution
+	$stmt = $pdo->prepare($sqlSearchResultAff);
+
+	$stmt->bindValue(':catsearch', '%'.$catSearch.'%');
+	$stmt->bindValue(':titlesearch', '%'.$titleSearch.'%');
+	$stmt->bindValue(':actorsearch', '%'.$actorSearch.'%');
+	$stmt->bindValue(':stosearch', '%'.$stoSearch.'%');
+	$stmt->bindValue(':datesearch', '%'.$dateSearch.'%');
+	if($stmt->execute() === false){
+		print_r($pdo->errorInfo());
+	}
+	else if($stmt->rowCount() > 0){
+		$searchList = $stmt->fetchAll();
+		print_r($searchList3);
+		//$nbRows = $stmt->rowCount();
+	}
+
+}
+
 
 require 'inc/header.php';
 require 'inc/search_view.php';
