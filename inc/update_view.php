b754a582e3c1bfb9af9e6d274b452f2563da6b75
Michel Cavro <mcavro@pt.lu>
2016-06-02 16:15:54 +0200

    WIP update & update_view

--- inc/update_view.php
+++ inc/update_view.php
@@ -1,20 +1,19 @@
-<?php
 
-require 'header.php';
-
-?>
 <form action="" method="post">
 	<fieldset>
 		<legend>Modifier les infos du film</legend>
-		<input type="text" name="mov_title" value="" placeholder="$Title" /><br />
-		<input type="text" name="mov_cast" value="" placeholder="$Cast" /><br />
-		<input type="text" name="mov_synopsis" value="" placeholder="$Synopsis" /><br />
-		<input type="text" name="mov_original_title" value="" placeholder="$OriginalTitle" /><br />
+		<input type="text" name="mov_title" value="<?= $movieSelect['mov_title'] ?>"/><br />
+		<select name="selection">
+	    <option selected="selected"><?= $movieSelect['cat_name']?></option>
+	    <option value="<?= $movieSelect['cat_id']?>"><?= $movieSelect['cat_name']?></option>
+		</select><br />
+		<!--<input type="text" name="cat_id" value="<?= $movieSelect['cat_name']?>"/><br />-->
+		<input type="text" name="mov_cast" value="<?= $movieSelect['mov_cast'] ?>"/><br />
+		<input type="text" name="mov_synopsis" value="<?= $movieSelect['mov_synopsis'] ?>"/><br />
+		<input type="text" name="mov_path" value="<?= $movieSelect['mov_path']?>"/><br />
+		<input type="text" name="mov_original_title" value="<?= $movieSelect['mov_original_title'] ?>"/><br />
+		<input type="text" name="mov_image" value="<?= $movieSelect['mov_image']?>"/><br />
+		<img height="120px" width="90px" src="<?= $movieSelect['mov_image']?>">
 		<input type="submit" value="Valider">
 	</fieldset>
 </form>
-<?php
-
-require 'footer.php';
-
-?>
