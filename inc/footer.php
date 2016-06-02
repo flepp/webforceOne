9c01e44f3a3ca23b934c49e50cd91e6a17d62d8c
pageioa <paul.ioanid@yahoo.com>
2016-06-02 10:21:31 +0200

    new changes

---
+++ inc/footer.php
@@ -0,0 +1,5 @@
+	<footer>
+		<p>Design by WebForceOne 2016</p>
+	</footer>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projet WebForceOne</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
