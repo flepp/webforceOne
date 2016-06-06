9a2410ac5f47e56184db43e4db199665e1b10122
pageioa <paul.ioanid@yahoo.com>
2016-06-06 10:14:26 +0200

    sending files category.php & category_view.php

--- inc/category_view.php
+++ inc/category_view.php
@@ -1,21 +1,26 @@
 <h1>Gestion des catégories</h1>
-<!-- menu déroulant des catégories -->
-<fieldset>
-	<select name="catégories">
-		<option value="1">Action</option>
-		<option value="2">Policier</option>
-		<option value="3">Science-fiction</option>
-		<option value="4">Thriller</option>
-		<option value="5">Horror</option>
-		<option value="6">Dessin animé</option>
-		<option value="7">Documentaire</option>
+<!-- Menu deroulant des categories -->
+<form action="" method="get">
+	<select name="cat_id">
+		<option value="0">ajout d'une catégorie</option>
+		<!-- Je fais le remplissage du menu deroulant -->
+		<?php foreach ($allCategoriesList as $currentCategory) : ?>
+		<option value="<?= $currentCategory['cat_id']; ?>"<?= $currentId == $currentCategory['cat_id']?>><?= $currentCategory['cat_name']; ?></option>
+		<?php endforeach; ?>
 	</select>
-</fieldset>	
+	<input type="submit" value="OK"/>
+</form>
 
 <form action="" method="post">
 	<fieldset>
-		<input type="text" name="key" value="" /><br />
-		<input type="text" name="value" value="" /><br />
+		<input type="text" name="cat_id" value="<?= $currentId; ?>"/><br />
+		<table>
+			<tr>
+				<td>Nom catégorie:</td>
+				<td><input type="text" name="cat_name" value="<?= $categoryName; ?>"/></td>
+			</tr>
+		</table>
 		<button type="submit">Valider</button>
 	</fieldset>
 </form>
+
